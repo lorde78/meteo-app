6178d8227834efeec5007edd5b535d4f6797c481
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-04-04 16:21:12 +0200

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState({});
+
+	const [location, setLocation] = useState("");
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=37b0716b0f1326c7b478239e6976fbd9`;
+
+	const searchLocation = (event) => {
+		if (event.key === "Enter") {
+			event.preventDefault();
+			axios.get(url).then((response) => {
+				setData(response.data);
+				console.log(response.data);
+			});
+			setLocation("");
+		}
+	};
+
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>Recherchez une ville</h1>
+				<form action="">
+					<input
+						type="text"
+						value={location}
+						onChange={(event) => setLocation(event.target.value)}
+						onKeyPress={searchLocation}
+					/>
+				</form>
+				<div>
+					{data.name ? <p>A {data.name},</p> : null }
+				</div>
+				<div>{data.main ? <p>il fait{data.main.temp} degree</p> : null}</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
